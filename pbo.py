6dfd0fcbc82273422f599dc172b1618cbfb6fa9c
hafizhnawawi <122343835+hafizhnawawi@users.noreply.github.com>
2023-02-21 14:38:01 +0700

    Add files via upload

---
+++ pbo.py
@@ -0,0 +1,12 @@
+def __init__(self, alas, tinggi):
+    self.alas = alas
+    self.tinggi = tinggi
+
+def get_luas(self):
+    return 0.5 * self.alas * self.tinggi
+
+segitiga1 = segitiga(5, 10)
+
+print('luas segitiga:', segitiga1.get_luas())
+print('luas segitiga:', segitiga1.get_luas())
+print('luas segitiga:', segitiga1.get_luas())
